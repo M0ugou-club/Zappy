6a803013b5ab1615d55dbfbe2f6eb1629d4d2dd8
Klaiveft <pierre.bourgery@epitech.eu>
2024-05-23 15:36:12 +0200

    ADD: -help

--- IA/src/connexion.py
+++ IA/src/connexion.py
@@ -10,6 +10,17 @@ H_HELP = "is the name of the machine; localhost by default"
 if len(sys.argv) < 2:
     sys.exit(84)
 
+if len(sys.argv) > 6:
+    sys.exit(84)
+
+if sys.argv[1] == "-help":
+    print("USAGE: ./zappy_ai -p port -n name -h machine")
+    print("DESCRIPTION:")
+    print(f"    -p port     {P_HELP}")
+    print(f"    -n name     {N_HELP}")
+    print(f"    -h machine  {H_HELP}")
+    sys.exit(0)
+
 def main():
     '''main function'''
     print("test main")
