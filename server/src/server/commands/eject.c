c9b619f3c8b74fd5a3e74608812e919ead61a862
Kapsulon <theo.bary@epitech.eu>
2024-06-07 15:26:43 +0200

    FIX: coding style

--- server/src/server/commands/eject.c
+++ server/src/server/commands/eject.c
@@ -7,7 +7,8 @@
 
 #include "server.h"
 
-// Eject all players on the same tile as the player in the direction they are facing
+// Eject all players on the same tile
+// as the player in the direction they are facing
 void cmd_eject(server_t *srv, connection_t *cl, regex_parse_t *parse)
 {
     return;
