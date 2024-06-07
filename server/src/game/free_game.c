4323ab6eb27b27c8964bf2fc1bfe2b8876528d03
PaulArbez <paul.arbez@epitech.eu>
2024-06-07 12:05:18 +0200

    ADD: free eggs

--- server/src/game/free_game.c
+++ server/src/game/free_game.c
@@ -7,9 +7,22 @@
 
 #include "game.h"
 
+static free_eggs(square_t *square)
+{
+    for (int i = 0; square->eggs[i]; i++)
+        free(square->eggs[i]);
+    free(square->eggs);
+}
+
 void free_game(game_t *game)
 {
+    for (int i = 0; i < game->map_x; i++) {
+        for (int j = 0; j < game->map_y; j++)
+            free_eggs(&game->map[i][j]);
+        free(game->map[i]);
+    }
     free(game->map);
+    free(game->max_items);
     free(game->players);
     free(game);
 }
