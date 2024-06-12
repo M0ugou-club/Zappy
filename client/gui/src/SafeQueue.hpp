dd052fc23cf6481382e8ccaa95f6e614e3230a6e
Kapsulon <theo.bary@epitech.eu>
2024-06-12 10:55:47 +0200

    ADD: safe queue

--- client/gui/src/SafeQueue.hpp
+++ client/gui/src/SafeQueue.hpp
@@ -19,8 +19,24 @@ class SafeQueue {
         SafeQueue(const SafeQueue &other) = default;
         ~SafeQueue() = default;
 
-        void enqueue(T t);
-        T dequeue();
+        void enqueue(T t)
+        {
+            std::unique_lock<std::mutex> lock(_mutex);
+            _queue.push(t);
+            _cond.notify_one();
+        }
+
+        T dequeue()
+        {
+            std::unique_lock<std::mutex> lock(_mutex);
+            while (_queue.empty()) {
+                _cond.wait(lock);
+            }
+            T val = _queue.front();
+            _queue.pop();
+            return val;
+        }
+
         bool empty() { return _queue.empty(); }
 
     private:
