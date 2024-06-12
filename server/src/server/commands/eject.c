80a50710dd2037f9599bf70f4c932265eac2eb0a
PaulArbez <paul.arbez@epitech.eu>
2024-06-12 12:09:33 +0200

    ADD: ejection on server

--- server/src/server/commands/eject.c
+++ server/src/server/commands/eject.c
@@ -11,5 +11,22 @@
 // as the player in the direction they are facing
 void cmd_eject(server_t *srv, connection_t *cl, regex_parse_t *parse)
 {
-    return;
+    player_t *player = get_player_by_fd(cl, srv);
+    player_t *tmp = srv->game->players;
+
+    while (tmp != NULL) {
+        if (tmp->square == player->square && tmp != player) {
+            if (tmp->direction == NORTH) {
+                tmp->square = tmp->square->south;
+            } else if (tmp->direction == SOUTH) {
+                tmp->square = tmp->square->north;
+            } else if (tmp->direction == EAST) {
+                tmp->square = tmp->square->west;
+            } else if (tmp->direction == WEST) {
+                tmp->square = tmp->square->east;
+            }
+        }
+        tmp = tmp->next;
+    }
+    player->square->eggs = NULL;
 }
