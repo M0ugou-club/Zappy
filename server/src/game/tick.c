f2e071769e8cf99c102aab5e5416a4dfee19cb2e
Kapsulon <theo.bary@epitech.eu>
2024-06-18 16:32:55 +0200

    FIX: coding style

--- server/src/game/tick.c
+++ server/src/game/tick.c
@@ -11,7 +11,8 @@ static bool eat(server_t *srv, player_t *ply)
 {
     connection_t *tmp = get_client_by_fd(srv->cons, ply->fd);
 
-    if (difftime(time(NULL), ply->last_eat) >= EAT_TIME / srv->args->frequency) {
+    if (difftime(time(NULL), ply->last_eat)
+        >= EAT_TIME / srv->args->frequency) {
         if (ply->inventory[FOOD] > 0) {
             ply->inventory[FOOD] -= 1;
             ply->last_eat = time(NULL);
@@ -26,9 +27,9 @@ static bool eat(server_t *srv, player_t *ply)
 
 void game_tick(server_t *srv)
 {
-    srv->game->tick++;
     player_t *ply = srv->game->players;
 
+    srv->game->tick++;
     while (ply != NULL) {
         ply->disconnect = !eat(srv, ply);
         ply = ply->next;
