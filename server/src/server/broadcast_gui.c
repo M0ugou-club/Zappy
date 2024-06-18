2ef387c11af640bea84ef45d2e6f952a9e89a934
PaulArbez <paul.arbez@epitech.eu>
2024-06-18 12:00:16 +0200

    ADD: broadcast_gui

---
+++ server/src/server/broadcast_gui.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy [WSL: Fedora]
+** File description:
+** broadcast_gui
+*/
+
+#include <stdarg.h>
+#include <stdio.h>
+#include "server.h"
+
+void broadcast_gui(server_t *srv, char *format, ...)
+{
+    va_list args;
+    char *msg = NULL;
+
+    va_start(args, format);
+    vasprintf(&msg, format, args);
+    va_end(args);
+    for (connection_t *tmp = srv->cons; tmp; tmp = tmp->next) {
+        if (strcmp(tmp->team, "GRAPHIC") == 0)
+            queue_message(tmp, msg);
+    }
+    free(msg);
+}
