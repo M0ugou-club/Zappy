9d7d04ba8ff7dae701e56c149f04ad34026f7607
Klaiveft <pierre.bourgery@epitech.eu>
2024-06-20 10:51:58 +0200

    FIX: coding-style

--- server/src/server/commands/look.c
+++ server/src/server/commands/look.c
@@ -55,7 +55,7 @@ static square_t *mv_gap(square_t *square, direction_t orientation, int gap)
     return square;
 }
 
-static square_t *get_square_start(player_t *player, int cone_gap, int cone_distance)
+static square_t *get_start(player_t *player, int cone_gap, int cone_distance)
 {
     direction_t orientation = player->direction;
     square_t *square = player->square;
@@ -65,7 +65,8 @@ static square_t *get_square_start(player_t *player, int cone_gap, int cone_dista
     return square;
 }
 
-int count_players_on_square(square_t *square, server_t *srv) {
+int count_players_on_square(square_t *square, server_t *srv)
+{
     player_t *players = srv->game->players;
     int players_count = 0;
 
@@ -78,7 +79,8 @@ int count_players_on_square(square_t *square, server_t *srv) {
     return players_count;
 }
 
-static void append_items_of_type(char *response, int item_count, const char *item_name) {
+static void append_items(char *response, int item_count, const char *item_name)
+{
     for (int j = 0; j < item_count; j++) {
         if (strlen(response) > 0) {
             strcat(response, " ");
@@ -87,7 +89,8 @@ static void append_items_of_type(char *response, int item_count, const char *ite
     }
 }
 
-char* get_square_content(square_t *square, server_t *srv) {
+char* get_square_content(square_t *square, server_t *srv)
+{
     static char response[1024];
     int players_count = count_players_on_square(square, srv);
 
@@ -100,7 +103,7 @@ char* get_square_content(square_t *square, server_t *srv) {
         players_count--;
     }
     for (int i = 0; i < 7; i++) {
-        append_items_of_type(response, square->items[i], objects[i]);
+        append_items(response, square->items[i], objects[i]);
     }
     return response;
 }
@@ -118,17 +121,17 @@ static square_t *move_square_widht(square_t *square, direction_t orientation)
     return square;
 }
 
-char* get_lines_squares(player_t *player, int cone_width, square_t *square_start, server_t *srv)
+char* get_ln_squa(player_t *p, int cn_width, square_t *sq_start, server_t *srv)
 {
     static char response[1024];
-    direction_t orientation = player->direction;
-    square_t *square = square_start;
+    direction_t orientation = p->direction;
+    square_t *square = sq_start;
 
     strcpy(response, "");
-    for (int i = 0; i < cone_width; i++) {
+    for (int i = 0; i < cn_width; i++) {
         if (square != NULL)
             strcat(response, get_square_content(square, srv));
-        if (i != cone_width - 1)
+        if (i != cn_width - 1)
             strcat(response, ",");
         if (square != NULL) {
             square = move_square_widht(square, orientation);
@@ -148,10 +151,10 @@ void cmd_look(server_t *srv, connection_t *cl, regex_parse_t *parse)
     strcpy(response, "[");
     strcat(response, get_square_content(player->square, srv));
     for (int i = 1; i <= player->level; i++) {
-        square_t *start_line = get_square_start(player, cone_gap, cone_distance);
+        square_t *start_line = get_start(player, cone_gap, cone_distance);
         strcat(response, ",");
         if (start_line != NULL) {
-            strcat(response, get_lines_squares(player, cone_width, start_line, srv));
+            strcat(response, get_ln_squa(player, cone_width, start_line, srv));
         }
         cone_width += 2;
         cone_gap += 1;
